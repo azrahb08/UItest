655707f14b61192b07f81dd874ae23f8823edad0
azrahb08 <azrahb08@gmail.com>
2019-05-26 14:27:21 +0200

    Minor changes for Iteration 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Location.create! ([
-  {"name": "Buckingham Palace", "latitude": "51.501564", "longitude": "-0.141944"}
-  ])
+# Location.create! ([
+#   {"name": "Buckingham Palace", "latitude": "51.501564", "longitude": "-0.141944"}
+#   ])
 
-Event.create([
-  {"name":"Test","description":"location test", "address":"Buckingham Palace", "latitude":"51.501564","longitude":"-0.141944"}
-  ])
+# Event.create([
+#   {"name":"Test","description":"location test", "address":"Buckingham Palace", "latitude":"51.501564","longitude":"-0.141944"}
+#   ])
 
-  50.times do
-      Guestlist.create(
-      name: Faker::Name.name_with_middle,
-      email: Faker::Internet.free_email,
-      response: Faker::Coin.flip
-    )
+  # 50.times do
+  #     Guestlist.create(
+  #     name: Faker::Name.name_with_middle,
+  #     email: Faker::Internet.free_email,
+  #     response: Faker::Coin.flip
+  #   )
     #name = Faker::Coffee.blend_name
     #quantity = rand(0..100)
     #Guestlist.create(name: name, email: email, response: response)
 
-  end
+    # todo_list = [
+    #   [ 'Make Hair Appointment',  ],
+    #   [ 'Buy outfit', 'Shop at Canal Walk and Waterfront' ],
+    #   [ 'Arrange centerpieces', 'Call flower lady' ]
+    # ]
+    #
+    # todo_list.each do |title, description|
+    #   Todo.create( title: title, description: description )
+    # end
+#     Todo.destroy_all
+#     Todo.create!([{
+#       title: "Make Hair Appointment",
+#       description: "Phone Scar Hair for Kertain treatment"
+#     },
+#     {
+#       title: "Order food",
+#       description: "Contact caterer to organised menus"
+#     },
+#     {
+#       title: "Buy outfit",
+#       description: "Shop at Canal Walk and Waterfront"
+#     },
+#     {
+#       title: "Arrange centerpieces",
+#       description: "Call flower lady"
+#     }])
+# p "Created #{Todo.count} todos"
